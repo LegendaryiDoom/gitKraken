7d4cda3cb933d9f626cc738fa0965e29fc1f1114
AlanOC <alanortegonc@hotmail.com>
2019-11-01 15:15:10 -0600

    First Commit

---
+++ lib/formulario.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegistroaPage extends StatefulWidget {
+  RegistroaPage({Key key}) : super(key: key);
+
+  @override
+  _RegistroaPageState createState() => _RegistroaPageState();
+}
+
+class _RegistroaPageState extends State<RegistroaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text("Hola"),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_uno/formulario.dart';
+
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        'registro': (BuildContext context) => RegistroaPage(),
+        'home'  : (BuildContext context) => MyApp(),
+      },
+      title: 'Material App',
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          tooltip: "Hola",
+          child: Icon(Icons.add),
+          onPressed: (){
+            Navigator.pushNamed(context, 'registro');
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+        app
+        floatingActionButton: FloatingActionButton(
+          tooltip:  "Holaaa",
+          child: Icon(Icons.add),
+          onPressed: (){
+            print("Hola");
+
+          },
+        ),
+
+      ),
+    );
+  }
+}
